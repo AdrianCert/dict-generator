756c56561834856bd00e9a0c9a65f92d9509c84a
Panaintescu Adrian Valentin <adrian.cert@gmail.com>
2024-04-07 19:49:34 +0300

    Update server.py to support different kinds of data generation and import/export functionality

--- src/dict_generator/app/server.py
+++ src/dict_generator/app/server.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, Response
 
+import random
 import json
 
 from dict_generator.generator import (
@@ -35,7 +36,8 @@ def home():
             }
             dist_schema[name] = float(frequency) / 100
 
-        count = request.form.get("count") or 100
+        count = request.form.get("count") or 1000
+        kind = request.form.get("kind") or "pub"
 
         def generate():
             key_generator = FrequencyBasedKeyGenerator(int(count), dist_schema)
@@ -45,7 +47,60 @@ def home():
                 data = dict_generator.generate(item)
                 yield json.dumps(data) + "\n"
 
-        return Response(generate(), mimetype="application/json")
+        def generate_subscription():
+            key_generator = FrequencyBasedKeyGenerator(int(count), dist_schema)
+            dict_generator = SchemaBasedDictGenerator(schema)
+            for item in key_generator:
+                # Replace this with your actual data generation logic
+                data = dict_generator.generate(item)
+                result = []
+                for key, value in data.items():
+                    operand = "="
+                    if schema[key]["type"] in ["int", "float"]:
+                        operand = random.choice(["<", ">", "=", "<=", ">="])
+                    # result.append((key, operand, value))
+                    result.append(f"({key}, {operand}, {value})")
+
+                yield "{" + ", ".join(result) + "}\n"
+
+        def dummy_generate():
+            key_generator = FrequencyBasedKeyGenerator(int(count), dist_schema)
+            dict_generator = SchemaBasedDictGenerator(schema)
+            result = ""
+            for item in key_generator:
+                # Replace this with your actual data generation logic
+                data = dict_generator.generate(item)
+                result = result + json.dumps(data) + "\n"
+            yield result
+
+        def dummy_generate_subscription():
+            key_generator = FrequencyBasedKeyGenerator(int(count), dist_schema)
+            dict_generator = SchemaBasedDictGenerator(schema)
+            result_ = ""
+            for item in key_generator:
+                # Replace this with your actual data generation logic
+                data = dict_generator.generate(item)
+                result = []
+                for key, value in data.items():
+                    operand = "="
+                    if schema[key]["type"] in ["int", "float"]:
+                        operand = random.choice(["<", ">", "=", "<=", ">="])
+                    # result.append((key, operand, value))
+                    result.append(f"({key}, {operand}, {value})")
+
+                result_ = result_ + "{" + ", ".join(result) + "}\n"
+            yield result_
+
+        if kind == "pub":
+            return Response(generate(), mimetype="application/json")
+        if kind == "sub":
+            return Response(generate_subscription(), mimetype="application/json")
+        if kind == "dummy_pub":
+            return Response(dummy_generate(), mimetype="application/json")
+        if kind == "dummy_sub":
+            return Response(dummy_generate_subscription(), mimetype="application/json")
+
+        return "Invalid kind of data"
     return render_template("index.html")
 
 
